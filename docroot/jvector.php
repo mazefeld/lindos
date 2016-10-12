b89e0c4430602ca28d27aac5fad6be2dceaca111
Michael Hatzfeld <michaelhatzfeld@gmail.com>
2016-10-12 21:46:19 +0200

    REMOVED unused minified vector ADDED jvector dev file

--- docroot/jvector.php
+++ docroot/jvector.php
@@ -7,50 +7,17 @@
 
 <!--    <script src="lib/jquery/dist/jquery.js"></script>-->
     <script src="http://jvectormap.com/js/jquery-1.8.2.min.js"></script>
-    <script src="js/jquery-jvectormap-2.0.3.min.js"></script>
-<!---->
-<!--    <script src="lib/jvectormap/src/jvectormap.js"></script>-->
-<!--    <script src="lib/jvectormap/src/abstract-canvas-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/abstract-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/abstract-shape-element.js"></script>-->
-<!---->
-<!---->
-<!--    <script src="lib/jvectormap/src/map.js"></script>-->
-<!--    <script src="lib/jvectormap/src/vector-canvas.js"></script>-->
-
-<!---->
-<!---->
-<!--    <script src="lib/jvectormap/src/color-scale.js"></script>-->
-<!--    <script src="lib/jvectormap/src/data-series.js"></script>-->
-<!--    <script src="lib/jvectormap/src/legend.js"></script>-->
-<!--    <script src="lib/jvectormap/src/map-object.js"></script>-->
-<!--    <script src="lib/jvectormap/src/marker.js"></script>-->
-<!--    <script src="lib/jvectormap/src/numeric-scale.js"></script>-->
-<!--    <script src="lib/jvectormap/src/ordinal-scale.js"></script>-->
-<!--    <script src="lib/jvectormap/src/proj.js"></script>-->
-<!--    <script src="lib/jvectormap/src/region.js"></script>-->
-<!--    <script src="lib/jvectormap/src/simple-scale.js"></script>-->
-<!---->
-<!--    <script src="lib/jvectormap/src/svg-canvas-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-circle-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-group-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-image-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-path-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-shape-element.js"></script>-->
-<!--    <script src="lib/jvectormap/src/svg-text-element.js"></script>-->
-<!---->
-<!---->
-<!---->
-<!--    <script src="lib/jvectormap/jquery-jvectormap.js"></script>-->
+    <script src="js/jvector.dev.js"></script>
 
     <script src="assets/data/jvectormap-world-mill.js"></script>
     <script src="http://jvectormap.com/js/jquery-jvectormap-de-merc.js"></script>
+
+    <script type="text/javascript" src="js/jvector-custom.js" ></script>
 </head>
 <body>
 <div id="map" class="map"></div>
 
-<script type="text/javascript" src="js/jvector-custom.js" ></script>
+
 </body>
 
 </html>
